7053afe73dde69af70ee127a4edd00045248704e
unknown <tiamsw@163.com>
2013-04-13 00:47:45 +0800

    iiiii

--- user_books_show.php
+++ user_books_show.php
@@ -6,27 +6,43 @@
   <div class="startbut">
         <a href="#">Start a New Book</a>
      </div>
+     
+     <?php 
+     global $wpdb;
+     
+     $sql = <<<SQL
+		select 	te.`name` bookname ,tx.description bookdes ,org.icon bookico ,tx.parent parent ,tx.term_taxonomy_id shipid 
+		from wp_terms te 
+		JOIN 			wp_term_taxonomy tx 	on te.term_id = tx.term_id and tx.taxonomy = 'series' 
+		LEFT JOIN wp_orgseriesicons org on org.term_id = te.term_id
+SQL;
+     $books = $wpdb->get_results($sql);
+     foreach ($books as $book) {
+     	$catname = "";
+     	$catSql = "select te.`name`  catname from wp_terms te ,wp_term_taxonomy tx where tx.term_id = te.term_id and tx.term_taxonomy_id = '".$book->parent."'";
+     	$cats = $wpdb->get_results($catSql);
+     	foreach ($cats as $cat) {
+     		$catname = $cat->catname;
+     	}
+     	//
+     	
+     ?>
+     
     <div id="conter"class="bookcontent fl"> 
-    	<div class="usertitle">Life Plan<span>Last Update:<font>13/03/2013</font></span></div> 
+    	<div class="usertitle"><?php echo $book->bookname ;?><span>Last Update:<font>13/03/2013</font></span></div> 
         <div class="mark fl">
         	<a href="#" class="viewbook">View Book</a>
         </div> 
         <div class="bookcontentbox">
-        	<a href="#"><img src="images/photo01.jpg" class="fl" width="181"  height="270"/></a>
+        	<a href="#"><img src="<?php echo $book->bookico ;?>" class="fl" width="181"  height="270"/></a>
             <p>
-            	Captain Hesperus, a grey furry feline from Orrira, wiped goat soup 
-                from his eyes and sighed. A torsion wave, burped out from the badly 
-                maintained engines of the Dubious Profit, had slid through the ship's 
-                mess just as he and his crew were sitting down to dine. Local s
-                pacetime in the cramped little room had briefly bulged and twisted, 
-                gravity pulling six ways at once. A nauseous sensation at the best of 
-                times, and possibly at its most inconvenient when one was seated in 
-                front of a large bowl of hot and oily soup...
+            	<?php echo $book->bookdes ;?>
             </p>
             <ul>
             	<li><strong>Category:</strong>
                 	<div class="bookmenubut">
-                    	<span>Space Opera</span>
+                    	<span><?php echo $catname;?></span>
+                    	<!-- 
                         <ul class="bookmenu">
                             <li><a href="#">Apocalypic and Post-apocalyptic</a></li>
                             <li><a href="#">Biopunk</a></li>
@@ -35,6 +51,7 @@
                             <li><a href="#">space opera</a></li>
                             <li><a href="#">superheroes</a></li>
                         </ul>
+                         -->
                     </div>
                 </li>
                 <li><strong>Words:</strong>17766</li>
@@ -62,93 +79,36 @@
                 <li class="titleWords">Words</li> 
                 <li class="titleLast">Last Upadte</li> 
             </ul>
+            <?php 
+            $chapterSql = <<<SQL
+		     	select post.ID id, post.post_name postname,DATE_FORMAT(post.post_modified,'%d/%m/%Y') postdate 
+		     	from wp_term_relationships ship
+		     	JOIN wp_posts post
+		     	on post.ID = ship.object_id and
+		     	post.post_type = 'post' and
+		     	post.post_status  = 'publish'
+SQL;
+            $chapterSql = $chapterSql." and ship.term_taxonomy_id = '".$book->shipid."'";
+            $chapters = $wpdb->get_results($chapterSql);
+            $chapterIndex = 0;
+            foreach ($chapters as $chapter) {
+            	$chaptername = $chapter->postname ;
+            	$chapterdate = $chapter->postdate ;
+            ?>
             <ul>
-            	<li class="titleChapter">1</li>
-                <li class="titleContent2">Torsion Wave<a href="#">edit</a></li>
+            	<li class="titleChapter"><?php echo ++$chapterIndex;?></li>
+                <li class="titleContent2"><?php echo $chaptername;?><a href="#">edit</a></li>
                 <li class="titleWords">1221</li> 
-                <li class="titleLast">13/03/2013</li> 
+                <li class="titleLast"><?php echo $chapterdate;?></li> 
             </ul>
-           <ul>
-            	<li class="titleChapter">1</li>
-                <li class="titleContent2">Torsion Wave<a href="#">edit</a></li>
-                <li class="titleWords">1221</li> 
-                <li class="titleLast">13/03/2013</li> 
-            </ul> 
+           <?php }?>
         </div>
         <div class="total">
         	<a href="#"><font>Add a New CHapter</font> </a>
         </div>
     </div> 
-    <div id="conter"class="bookcontent fl mt15"> 
-    	<div class="usertitle">Life Plan<span>Last Update:<font>13/03/2013</font></span></div> 
-        <div class="mark fl">
-        	<a href="#" class="viewbook">View Book</a>
-        </div> 
-        <div class="bookcontentbox">
-        	<a href="#"><img src="images/photo01.jpg" class="fl" width="181"  height="270"/></a>
-            <p>
-            	Captain Hesperus, a grey furry feline from Orrira, wiped goat soup 
-                from his eyes and sighed. A torsion wave, burped out from the badly 
-                maintained engines of the Dubious Profit, had slid through the ship's 
-                mess just as he and his crew were sitting down to dine. Local s
-                pacetime in the cramped little room had briefly bulged and twisted, 
-                gravity pulling six ways at once. A nauseous sensation at the best of 
-                times, and possibly at its most inconvenient when one was seated in 
-                front of a large bowl of hot and oily soup...
-            </p>
-            <ul>
-            	<li><strong>Category:</strong>
-                	<div class="bookmenubut">
-                    	<span>Space Opera</span>
-                        <ul class="bookmenu">
-                            <li><a href="#">Apocalypic and Post-apocalyptic</a></li>
-                            <li><a href="#">Biopunk</a></li>
-                            <li><a href="#">military science fiction </a></li>
-                            <li><a href="#">time travel</a></li>
-                            <li><a href="#">space opera</a></li>
-                            <li><a href="#">superheroes</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li><strong>Words:</strong>17766</li>
-                <li><strong>Progress:</strong>
-                	<div class="bookmenubut">
-                    	<span>Finished</span>
-                        <ul class="bookmenu">
-                            <li><a href="#">In-Progress</a></li>
-                            <li><a href="#">Finished</a></li> 
-                        </ul>
-                    </div>
-                </li>
-                <li><strong>Tags:</strong>
-                	<span style="display:table; margin-top:10px;">Captain hesperus,Feline, Qudira</span>
-                </li>
-            </ul>
-        </div>
-         <div class="startbut bor-top">
-            <a href="#">Update Book lnfo.</a>
-         </div>
-        <div class="bookboxlist">
-        	<ul class="list-title">
-            	<li class="titleChapter">Chapter</li>
-                <li class="titleContent2" style="text-align:center;">Content</li>
-                <li class="titleWords">Words</li> 
-                <li class="titleLast">Last Upadte</li> 
-            </ul>
-            <ul>
-            	<li class="titleChapter">1</li>
-                <li class="titleContent2">Torsion Wave<a href="#">edit</a></li>
-                <li class="titleWords">1221</li> 
-                <li class="titleLast">13/03/2013</li> 
-            </ul>
-           <ul>
-            	<li class="titleChapter">1</li>
-                <li class="titleContent2">Torsion Wave<a href="#">edit</a></li>
-                <li class="titleWords">1221</li> 
-                <li class="titleLast">13/03/2013</li> 
-            </ul> 
-        </div>
-        <div class="total"> 
-        </div>
-    </div>
+    <?php 
+     }
+     ?>
+
 <?php get_footer(); ?>
