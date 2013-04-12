0bb2ee78ce02f2d51481000f3007551ae19aea33
superyuyue <superyuyue@126.com>
2013-04-12 10:33:41 +0800

    modify forum

--- header.php
+++ header.php
@@ -47,12 +47,19 @@
 			<input type="hidden" name="instance" value="1" />
         </form> 
         <?php }else{?>
-        	<ul class="login">
+        	<!-- <ul class="login">
         	 <li>       welcome  <b><?php echo$current_user->user_login; ?></b> </li>
         	 <li><a href="<?php get_site_url(); ?>/wp-admin">Dashboard</a></li>
         	 <li><a href="?page_id=4&action=profile">Profile</a></li>
         	 <li><a href="<?php echo wp_logout_url(); ?>">Login out</a></li>
-        	 </ul>
+        	 </ul> -->
+        	 <ul class="userlogin">
+            	<li><img src="<?php echo get_template_directory_uri(); ?>/images/suerimg.jpg" /></li>
+              
+                <li><a href="<?php get_site_url(); ?>/wp-admin"><?php echo$current_user->user_login; ?> Dashboard</a></li>
+                <li class="active"><a href="#">My Book</a></li> 
+                <li><a href="<?php echo wp_logout_url(); ?>">Log Out</a></li>  
+            </ul>
        <?php }?> 
             <div class="searchform">
             		<form id="search_form" method="get" action="<?php echo home_url(); ?>" >
